0cfcd56c158a7bf6388e41df4f154b34fd5c1847
urvashi <rawaturvashi68@gmail.com>
2023-03-11 22:00:44 +0530

    handling GET request

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 import express from 'express';
+import fs from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.get('/', (request, response) => {
-  response.status(200).json({ message: 'hello' });
+// good practice to specify api version like v1
+// (req,res) is route handler
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours,
+    },
+  });
 });
 
 app.listen(port, () => {
-  console.log('app runnin on port 3000');
+  console.log('app running');
 });
