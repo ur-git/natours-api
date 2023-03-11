884cb82ac81bb7f9669f971ca81f7f84dc237842
urvashi <rawaturvashi68@gmail.com>
2023-03-11 22:31:01 +0530

    handling POST request

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+import exp from 'constants';
 import express from 'express';
 import fs from 'fs';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 const app = express();
+
+//middleware
+app.use(express.json());
+
 const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -20,6 +24,23 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour)
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours),(err)=>{
+    res.status(201).send({
+        status:'success',
+        data:{
+            tours:newTour
+        }
+    })
+  })
+  console.log(req.body);
+//   res.send('done');
 });
 
 app.listen(port, () => {
